5829a7b565d6d72d4735659ca0628e9896075f03
Suyash Pratap Singh <sakshams1501@gmail.com>
2021-04-29 19:04:38 +0530

    adding icons and using flex and margins to position stuff

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
+import { View } from 'react-native';
 
 const TodoItem = (props) => {
 
     const { item, pressHandler } = props;
     return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>
-                {item.text}
-            </Text>
+        <TouchableOpacity onPress={() => pressHandler(item.key)} style={styles.container}>
+            <View style={styles.item}>
+                <Text style={styles.itemText}>
+                    {item.text}
+                </Text>
+                <MaterialIcons name="delete" size={18} color='#333' />
+            </View>
         </TouchableOpacity>
     )
 }
@@ -22,6 +27,12 @@ const styles = StyleSheet.create({
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
+        borderRadius: 10,
+        flex: 1,
+        flexDirection:'row'
+    },
+    itemText: {
+        marginRight:'auto'
+        
     }
 })
