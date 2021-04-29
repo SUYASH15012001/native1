aa4c778a6f9f57224e4784d9db09e1744bd2d0ad
Suyash Pratap Singh <sakshams1501@gmail.com>
2021-04-29 21:09:03 +0530

    ReviewsApp init

--- App.js
+++ App.js
@@ -1,59 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
-import Header from './components/Header';
-import TodoItem from './components/TodoItem';
-import AddTodo from './components/AddTodo';
-// import SandBox from './components/SandBox';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-  
-  
-  const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1' },
-    { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
-  ]);
-
-  const pressHandler = key => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo=> todo.key !== key)
-    })
-  }
-
-  const submitHandler = text => {
-    if(text.length>3){
-      setTodos((prevTodos) => {
-        return[
-          {text, key: Math.random().toString()},
-          ...prevTodos
-        ];
-      })
-    }
-    else {
-      Alert.alert('OOOPS!!!', 'todos must be 3 chars long atleast', [
-        {text:'Try again', onPress: () => console.log('alert closed')}
-      ])
-    }
-  }
 
   return (
-    // <SandBox/>
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <Header/>
-        <View style={styles.content}>
-          <View style={styles.list}>
-            <AddTodo submitHandler={submitHandler} />
-              <FlatList
-                data={todos}
-                renderItem={({item}) => (
-                  <TodoItem item={item} pressHandler={pressHandler}/>
-                )}
-              />
-          </View>
-        </View>
-        </View>
-      </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text>Start Coding</Text>
+    </View>
   );
 }
 
@@ -61,15 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  content: {
-    flex:1,
-    padding: 30
-  },
-  list: {
-    flex:1,
-    marginTop:20
-  }
 });
